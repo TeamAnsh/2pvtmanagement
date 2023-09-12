dd19eec4c0a0fa3588c70d7f55728d76c72f2560
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-12 08:49:56 +0530

    Update couples.py

--- Hiroko/modules/couples.py
+++ Hiroko/modules/couples.py
@@ -1,12 +1,16 @@
+import os 
 import random
-from datetime import datetime
-from pyrogram import filters
-from pyrogram.enums import ChatType
+from datetime import datetime 
+from telegraph import upload_file
+from PIL import Image , ImageDraw
+from pyrogram import *
+from pyrogram.types import *
+from pyrogram.enums import *
 from Hiroko import Hiroko
-from Hiroko.Helper.database.couplesdb import get_couple, save_couple
+from Hiroko.Helper.database import *
 
 
-# Date and time
+
 def dt():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M")
@@ -24,52 +28,122 @@ def dt_tom():
     )
     return a
 
-
-today = str(dt()[0])
 tomorrow = str(dt_tom())
+today = str(dt()[0])
 
 
-@Hiroko.on_message(filters.command(["couple", "couples"]))
-async def couple(_, message):
+
+
+@Hiroko.on_message(filters.command("couples", COMMAND_HANDLER))
+async def ctest(_, message):
+    cid = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
-        return await message.reply_text("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋs ɪɴ ɢʀᴏᴜᴘ.")
+        return await message.reply_text("This command only works in groups.")
     try:
-        chat_id = message.chat.id
-        is_selected = await get_couple(chat_id, today)
-        if not is_selected:
-            list_of_users = []
-            async for i in Hiroko.get_chat_members(message.chat.id, limit=50):
-                if not i.user.is_bot:
-                    list_of_users.append(i.user.id)
-            if len(list_of_users) < 2:
-                return await message.reply_text("ɴᴏᴛ ᴇɴᴏᴜɢʜ ᴜsᴇʀ")
-            c1_id = random.choice(list_of_users)
-            c2_id = random.choice(list_of_users)
-            while c1_id == c2_id:
-                c1_id = random.choice(list_of_users)
-            c1_mention = (await Hiroko.get_users(c1_id)).mention
-            c2_mention = (await Hiroko.get_users(c2_id)).mention
+       is_selected = await get_couple(cid, today)
+       if not is_selected:
+         msg = await message.reply_text("Generating Couples Image...")
+         #GET LIST OF USERS
+         list_of_users = []
 
-            couple_selection_message = f"""**⚜ Cᴏᴜᴘʟᴇs ᴏғ ᴛʜᴇ ᴅᴀʏ :**
+         async for i in app.get_chat_members(message.chat.id, limit=50):
+             if not i.user.is_bot:
+               list_of_users.append(i.user.id)
 
-{c1_mention} + {c2_mention} = ❤️‍🔥
-ɴᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴄᴀɴ ʙᴇ ᴄʜᴏsᴇɴ ᴀᴛ 12 ᴀᴍ {tomorrow}"""
-            await Hiroko.send_photo(message.chat.id,photo="https://telegra.ph/file/26fa1b4373717cee697be.jpg",caption=couple_selection_message)
-            couple = {"c1_id": c1_id, "c2_id": c2_id}
-            await save_couple(chat_id, today, couple)
+         c1_id = random.choice(list_of_users)
+         c2_id = random.choice(list_of_users)
+         while c1_id == c2_id:
+              c1_id = random.choice(list_of_users)
 
-        elif is_selected:
-            c1_id = int(is_selected["c1_id"])
-            c2_id = int(is_selected["c2_id"])
-            c1_name = (await Hiroko.get_users(c1_id)).mention
-            c2_name = (await Hiroko.get_users(c2_id)).mention
-            couple_selection_message = f"""⚜ Cᴏᴜᴘʟᴇs ᴏғ ᴛʜᴇ ᴅᴀʏ :
 
-{c1_name} + {c2_name} = ❤️‍🔥
-ɴᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴄᴀɴ ʙᴇ ᴄʜᴏsᴇɴ ᴀᴛ 12 ᴀᴍ {tomorrow}"""
-            await Hiroko.send_photo(message.chat.id,photo="https://telegra.ph/file/26fa1b4373717cee697be.jpg",caption=couple_selection_message)
+         photo1 = (await Hiroko.get_chat(c1_id)).photo
+         photo2 = (await Hiroko.get_chat(c2_id)).photo
+ 
+         N1 = (await Hiroko.get_users(c1_id)).mention 
+         N2 = (await Hiroko.get_users(c2_id)).mention
+         
+         try:
+            p1 = await Hiroko.download_media(photo1.big_file_id, file_name="pfp.png")
+         except Exception:
+            p1 = "Hiroko/Helper/resources/couples.png"
+         try:
+            p2 = await Hiroko.download_media(photo2.big_file_id, file_name="pfp1.png")
+         except Exception:
+            p2 = "Hiroko/Helper/resources/couples.png"
+            
+         img1 = Image.open(f"{p1}")
+         img2 = Image.open(f"{p2}")
+
+         img = Image.open("Hiroko/Helper/resources/couples.png")
+
+         img1 = img1.resize((320,320))
+         img2 = img2.resize((320,320))
+
+         mask = Image.new('L', img1.size, 0)
+         draw = ImageDraw.Draw(mask) 
+         draw.ellipse((0, 0) + img1.size, fill=255)
+
+         mask1 = Image.new('L', img2.size, 0)
+         draw = ImageDraw.Draw(mask1) 
+         draw.ellipse((0, 0) + img2.size, fill=255)
+
+
+         img1.putalpha(mask)
+         img2.putalpha(mask1)
+
+         draw = ImageDraw.Draw(img)
+
+         img.paste(img1, (90, 200), img1)
+         img.paste(img2, (871, 200), img2)
+
+         img.save(f'test_{cid}.png')
+    
+         TXT = f"""
+**TODAY'S SELECTED COUPLES 🎉 :
+➖➖➖➖➖➖➖➖➖➖➖➖
+{N1} + {N2} = ❣️
+➖➖➖➖➖➖➖➖➖➖➖➖
+NEXT COUPLES WILL BE SELECTED ON {tomorrow} !!**
+"""
+    
+         await message.reply_photo(f"test_{cid}.png", caption=TXT)
+         await msg.delete()
+         a = upload_file(f"test_{cid}.png")
+         for x in a:
+           img = "https://graph.org/" + x
+           couple = {"c1_id": c1_id, "c2_id": c2_id}
+           await save_couple(cid, today, couple, img)
+    
+         
+       elif is_selected:
+         msg = await message.reply_text("Getting Todays Couples Image...")
+         b = await _get_image(cid)
+         c1_id = int(is_selected["c1_id"])
+         c2_id = int(is_selected["c2_id"])
+         c1_name = (await Hiroko.get_users(c1_id)).first_name
+         c2_name = (await Hiroko.get_users(c2_id)).first_name
+         
+         TXT = f"""
+**TODAY'S SELECTED COUPLES 🎉 :
+➖➖➖➖➖➖➖➖➖➖➖➖
+[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❣️
+➖➖➖➖➖➖➖➖➖➖➖➖
+NEXT COUPLES WILL BE SELECTED ON {tomorrow} !!**
+"""
+         await message.reply_photo(b, caption=TXT)
+         await msg.delete()
     except Exception as e:
-        print(e)
-        await message.reply_text(e)
+        print(str(e))
+    try:
+      os.remove(f"./downloads/pfp1.png")
+      os.remove(f"./downloads/pfp2.png")
+      os.remove(f"test_{cid}.png")
+    except Exception:
+       pass
+         
+
+
+
+
 
 

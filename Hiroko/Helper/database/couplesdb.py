561d1485b2586bf2fb7058e2e0ac3cee35d5e859
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-12 08:55:06 +0530

    Update couplesdb.py

--- Hiroko/Helper/database/couplesdb.py
+++ Hiroko/Helper/database/couplesdb.py
@@ -7,28 +7,36 @@ mongo = MongoCli(MONGO_URL).Rankings
 coupledb = mongo.couple
 
    
-async def _get_lovers(chat_id: int):
-    lovers = await coupledb.find_one({"chat_id": chat_id})
-    if lovers and "couple" in lovers:
+async def _get_lovers(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
         lovers = lovers["couple"]
     else:
         lovers = {}
     return lovers
-    
 
-async def get_couple(chat_id: int, date: str):
-    lovers = await _get_lovers(chat_id)
+async def _get_image(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["img"]
+    else:
+        lovers = {}
+    return lovers
+
+async def get_couple(cid: int, date: str):
+    lovers = await _get_lovers(cid)
     if date in lovers:
-        return lovers["date"]
+        return lovers[date]
     else:
         return False
 
 
-async def save_couple(chat_id: int, date: str, couple: dict):
-    lovers = await _get_lovers(chat_id)
+async def save_couple(cid: int, date: str, couple: dict, img: str):
+    lovers = await _get_lovers(cid)
     lovers[date] = couple
     await coupledb.update_one(
-        {"chat_id": chat_id},
-        {"$set": {"couple": lovers}},
+        {"chat_id": cid},
+        {"$set": {"couple": lovers, "img": img}},
         upsert=True,
     )
+

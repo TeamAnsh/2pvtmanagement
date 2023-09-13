8ca8dcc72c6bd920c391fea59931c3b1a2841498
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-13 16:01:03 +0530

    Update couples.py

--- Hiroko/modules/couples.py
+++ Hiroko/modules/couples.py
@@ -8,7 +8,7 @@ from pyrogram.types import *
 from pyrogram.enums import *
 from Hiroko import Hiroko
 from Hiroko.Helper.database import *
-from Hiroko.Helper.database.couplesdb import _get_image
+from Hiroko.Helper.database.couplesdb import _get_image, get_couple, save_couple
 
 
 def dt():

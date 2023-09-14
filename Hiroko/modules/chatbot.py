a6ee35f7d30a24fbafdecfac5d2fb6e4c9f96afa
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-14 13:49:29 +0530

    Create chatbot.py

---
+++ Hiroko/modules/chatbot.py
@@ -0,0 +1,38 @@
+import requests
+from Hiroko import Hiroko
+from pyrogram import Client, filters
+
+
+
+api_key = "BLUE-AI-25154789-6280048819-123-white-kazu-6280048819"  # Replace with your API key
+
+
+def get_response(user_id, query):
+    params = {
+        "user_id": user_id,     # Replace with the user ID you want to use
+        "query": query          # Replace with your query
+    }
+
+    headers = {
+        "api_key": api_key
+    }
+
+    response = requests.get("https://blue-api.vercel.app/chatbot1", params=params, headers=headers)
+    return response.text
+
+
+
+@Hiroko.on_message(filters.command("chat", prefixes="/"))
+def chat(client, message):
+    reply = message.reply_to_message
+    query = message.text.split("/chat", maxsplit=1)[1].strip()
+    response = get_response(message.from_user.id, query)
+    result = response.json()
+    if not reply:
+      await message.reply_text(result["result"]["text"])
+    await reply.reply(result["result"]["text"])
+    
+
+
+
+

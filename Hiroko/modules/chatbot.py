9c8b18ae760f8eb3bb0f3bbbb843d5d6e3061a47
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-14 20:23:11 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -1,27 +1,7 @@
 import requests
 from Hiroko import Hiroko
-from pyrogram import filters, enums
-from config import MONGO_URL
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from pyrogram import filters
 
-mongo = MongoCli(MONGO_URL).Rankings
-
-db = mongo.chatbot
-
-
-def chatbot_db(chat_id: int):
-    x = db.find_one({"chat_id": chat_id})
-    if x:
-       return True
-    return False
-    
-def chatbot_on(chat_id: int):
-    format = {"chat_id": chat_id, "chatbot": "on"}
-    db.insert_one(format)
-
-def chatbot_off(chat_id: int):
-    if chatbot_db:
-       db.update_one({"chat_id": chat_id},{"$set":{"chatbot": "off"}})
 
 
 
@@ -40,41 +20,6 @@ def get_response(user_id, query):
 
     response = requests.get("https://blue-api.vercel.app/chatbot1", params=params, headers=headers)
     return response.json()
-
-
-
-
-
-
-@Hiroko.on_message(filters.command("chatbot"))
-async def chatbot(Hiroko, message):
-    user_id = message.from_user.id
-    chat_id = message.chat.id
-    if message.chat.type == enums.ChatType.PRIVATE:
-        if len(message.text) == 1:
-            return await message.reply_text("Format: /chatbot on|off")
-            if message.text.split(" ",1)[1] not in ["on","off"]:
-            
-        elif message.text.split(" ", 1)[1] == "on":
-            chatbot_on(chat_id)
-            return await message.reply_text("AI Enabled!")
-        elif message.text.split(" ",1)[1] == "off":
-            chatbot_off(chat_id)
-            return await message.reply_text("AI disabled!")      
-    else:
-        info = await message.chat.get_member(user_id)
-        if info.privileges.can_change_info:
-            if len(message.text) == 1:
-                return await message.reply_text("Format: /chatbot on|off")
-                if message.text.split(" ",1)[1] not in ["on","off"]:
-                
-            elif message.text.split(" ", 1)[1] == "on":       
-               chatbot_on(chat_id)
-               return await message.reply_text("AI Enabled!")
-            elif message.text.split(" ",1)[1] == "off":
-               chatbot_off(chat_id)
-               return await message.reply_text("AI disabled!")           
-            else: return await message.reply("fumk you, you are not admin")
 
 
 

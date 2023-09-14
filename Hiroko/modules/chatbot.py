e573265d6594c6635d7ea0d4f520176d4d58e744
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-14 18:59:14 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -30,8 +30,8 @@ api_key = "BLUE-AI-25154789-6280048819-123-white-kazu-6280048819"
 def get_response(user_id, query):
     params = {
         "user_id": user_id,
-        "query": query
-        "BOT_ID": 6632922889
+        "query": query,
+        "BOT_ID": BOT_ID
     }
 
     headers = {
@@ -72,12 +72,12 @@ async def chatbot(Hiroko, message):
 
 
 
-@Natasha.on_message(filters.text, group=200)
-async def chatbot_reply(natasha :Natasha, message):
+@Hiroko.on_message(filters.text, group=200)
+async def chatbot_reply(hiroko :Hiroko, message):
     if is_db:
-        bot_id = (await natasha.get_me()).id
+        BOT_ID = (await Hiroko.get_me()).id
         reply = message.reply_to_message
-        if reply and reply.from_user.id == bot_id:
+        if reply and reply.from_user.id == BOT_ID:
             query = message.text
             response = get_response(message.from_user.id, query)
             await message.reply_text(response["result"]["text"])

d755281a9b7764e7f5b57ad1b1e76244aa392535
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-14 18:28:47 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -3,12 +3,29 @@ from Hiroko import Hiroko
 from pyrogram import filters
 
 
+def is_db(chat_id: int):
+    x = db.find_one({"chat_id": chat_id})
+    if x:
+       return True
+    return False
+    
+def chatbot_on(chat_id: int):
+    format = {"chat_id": chat_id, "chatbot": "on"}
+    db.insert_one(format)
+
+def chatbot_off(chat_id: int):
+    if is_db:
+       db.update_one({"chat_id": chat_id},{"$set":{"chatbot": "off"}})
+
+
+
 api_key = "BLUE-AI-25154789-6280048819-123-white-kazu-6280048819"
 
 def get_response(user_id, query):
     params = {
         "user_id": user_id,
         "query": query
+        "BOT_ID": 6632922889
     }
 
     headers = {
@@ -17,6 +34,37 @@ def get_response(user_id, query):
 
     response = requests.get("https://blue-api.vercel.app/chatbot1", params=params, headers=headers)
     return response.json()
+
+
+@Hiroko.on_message(filters.command("chatbot"))
+async def chatbot(Hiroko, message):
+      user_id = message.from_user.id
+      if message.chat.type == enums.ChatType.PRIVATE:
+          if not ("on","off") in message.text.split(" ",1)[1]:
+             return await message.reply_text("Format: /chatbot on|off")
+          elif message.text.split(" ", 1)[1] == "on":
+               chatbot_on(message.chat.id)
+               return await message.reply_text("AI Enabled!")
+          elif message.text.split(" ",1)[1] == "off":
+               chatbot_off(message.chat.id)
+               return await message.reply_text("AI disabled!")      
+      else:
+        info = await message.chat.get_member(user_id)
+        if info.privileges in ("creator", "administrator"):
+           if not ("on","off") in message.text.split(" ",1)[1]:
+               return await message.reply_text("Format: /chatbot on|off")
+           elif message.text.split(" ", 1)[1] == "on":
+               chatbot_on(message.chat.id)
+               return await message.reply_text("AI Enabled!")
+           elif message.text.split(" ",1)[1] == "off":
+               chatbot_off(message.chat.id)
+               return await message.reply_text("AI disabled!")           
+        else: return await message.reply("fumk you, you are not admin")
+
+
+
+
+
 
 @Hiroko.on_message(filters.command("chatbot", prefixes="/"))
 async def chat(_, message):

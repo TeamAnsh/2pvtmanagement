9872754dbd3e33f769fcf17863990b39cffc5442
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-14 14:09:49 +0530

    Update image.py

--- Hiroko/modules/image.py
+++ Hiroko/modules/image.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, filters, types as t
-from Utils import getText,paginate_models,ImageGeneration
 from lexica import Client as ApiClient
 from pyrogram.types import InlineKeyboardButton
 from math import ceil
@@ -36,7 +35,33 @@ async def ImageGeneration(model,prompt):
         await client.close()
       
 
+# Copyright 2023 Qewertyy, MIT License
 
+async def getFile(message):
+    if not message.reply_to_message:
+        return None
+    if message.reply_to_message.document is False or message.reply_to_message.photo is False:
+        return None
+    if message.reply_to_message.document and message.reply_to_message.document.mime_type in ['image/png','image/jpg','image/jpeg'] or message.reply_to_message.photo:
+        image = await message.reply_to_message.download()
+        return image
+    else:
+        return None
+
+def getText(message):
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+        
 
 api = ApiClient()
 Models = api.getModels()['models']['image']

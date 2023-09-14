a3a03a07289ac8822012bae68fc622cc04e1a1d3
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-14 19:15:34 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -42,6 +42,10 @@ def get_response(user_id, query):
     return response.json()
 
 
+
+
+
+
 @Hiroko.on_message(filters.command("chatbot"))
 async def chatbot(Hiroko, message):
     user_id = message.from_user.id
@@ -56,11 +60,11 @@ async def chatbot(Hiroko, message):
         elif message.text.split(" ",1)[1] == "off":
             chatbot_off(chat_id)
             return await message.reply_text("AI disabled!")      
-        else:
-            info = await message.chat.get_member(chat_id, user_id)
-            if admin_check.privileges.can_change_info:
-                if not ("on","off") in message.text.split(" ",1)[1]:
-                    return await message.reply_text("Format: /chatbot on|off")
+    else:
+        info = await message.chat.get_member(chat_id, user_id)
+        if admin_check.privileges.can_change_info:
+            if not ("on","off") in message.text.split(" ",1)[1]:
+                return await message.reply_text("Format: /chatbot on|off")
             elif message.text.split(" ", 1)[1] == "on":       
                chatbot_on(chat_id)
                return await message.reply_text("AI Enabled!")

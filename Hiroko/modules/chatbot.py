b021558d8883f512efebd621cd409ec3b95e086c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-14 14:02:23 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -1,39 +1,34 @@
-import requests
 from Hiroko import Hiroko
-from pyrogram import Client, filters
+import requests
+from pyrogram import filters
 
 
-
-api_key = "BLUE-AI-25154789-6280048819-123-white-kazu-6280048819"  # Replace with your API key
+api_key = "BLUE-AI-25154789-6280048819-123-white-kazu-6280048819"
 
 
 def get_response(user_id, query):
     params = {
-        "user_id": user_id,     # Replace with the user ID you want to use
-        "query": query          # Replace with your query
+        "user_id": user_id,
+        "query": query
     }
-
     headers = {
         "api_key": api_key
     }
-
     response = requests.get("https://blue-api.vercel.app/chatbot1", params=params, headers=headers)
-    return response.text
+    return response.json()
 
 
 
 @Hiroko.on_message(filters.command("chat", prefixes="/"))
-def chat(client, message):
+async def chatbot(client, message):
     reply = message.reply_to_message
     query = message.text.split("/chat", maxsplit=1)[1].strip()
     response = get_response(message.from_user.id, query)
-    result = response.json()
-    if not reply:
-        await message.reply_text(result["result"]["text"])
-    else:
-        await reply.reply(result["result"]["text"])
-    
-
-
+    if response:
+        text = response["result"]["text"]
+        if not reply:
+            await message.reply_text(text)
+        else:
+            await reply.reply_text(text)
 
 

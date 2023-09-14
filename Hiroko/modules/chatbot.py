e9f7a5ec9d49575efee3fd10684bae4dda63e127
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-14 17:01:12 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -1,29 +1,29 @@
-from Hiroko import Hiroko
 import requests
-from pyrogram import filters
+from pyrogram import Client, filters
 
-
-api_key = "BLUE-AI-25154789-6280048819-123-white-kazu-6280048819"
-
+api_key = "BLUE-AI-25154789-6280048819-123-white-kazu-6280048819"  
 
 def get_response(user_id, query):
     params = {
-        "user_id": user_id,
-        "query": query
+        "user_id": user_id,      
+        "query": query           
     }
+
     headers = {
         "api_key": api_key
     }
+
     response = requests.get("https://blue-api.vercel.app/chatbot1", params=params, headers=headers)
-    return response.json()
+    return response.text
 
 
 
-@Hiroko.on_message(filters.command("chat", prefixes="/"))
-async def chatbot(client, message):
-    reply = message.reply_to_message
+@Natasha.on_message(filters.command("chat", prefixes="/"))
+def chat(client, message):
     query = message.text.split("/chat", maxsplit=1)[1].strip()
     response = get_response(message.from_user.id, query)
-    await message.reply(response["result"]["text"])
+    message.reply_text(response)
+
+
 
 

f2782a2fd4edf155d0a5291af74499b05ad2140c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-15 23:19:37 +0530

    Update music.py

--- Hiroko/modules/music.py
+++ Hiroko/modules/music.py
@@ -1,1 +1,12 @@
-hello
+from pyrogram import filters
+from Hiroko import Hiroko, userbot
+from pyrogram.types import Message
+
+
+@Hiroko.on_message(command.filters("join"))
+async def join_userbot(_,msg:Message):
+  chat_id = message.chat.id
+  invitelink = await Hiroko.export_chat_invite_link(chat_id)
+  await userbot.join_chat(invitelink)
+  await msg.reply("assistant successfully join.")
+

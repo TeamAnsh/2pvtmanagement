175c47c736847822a47b3302d96b99eaf3c22da4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-15 20:54:16 +0530

    Update pichatbot.py

--- Hiroko/modules/pichatbot.py
+++ Hiroko/modules/pichatbot.py
@@ -1,15 +1,13 @@
-from pyrogram import Client, filters
+import requests
+from pyrogram import filters
+from Hiroko import Hiroko
 
-# Define your API credentials and session name
-api_id = "YOUR_API_ID"
-api_hash = "YOUR_API_HASH"
-session_name = "your_session_name"
 
-# Create a Pyrogram client
-app = Client(session_name, api_id, api_hash)
 
-# Define the URL
+
 pi_url = "https://pi.ai/talk"
+
+
 
 # Function to interact with Pi AI and get a response
 async def chat_with_pi(message_text):
@@ -20,7 +18,7 @@ async def chat_with_pi(message_text):
         }
 
         # Send a POST request to the chat API
-        response = await app.session.post(pi_url, data=data)
+        response = await Hiroko.session.post(pi_url, data=data)
 
         if response.status_code == 200:
             # Extract and return the response text
@@ -32,14 +30,9 @@ async def chat_with_pi(message_text):
     except Exception as e:
         return f"Error: {str(e)}"
 
-# Define a command handler for handling incoming messages
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply("Hi there! Send me a message, and I'll chat with Pi AI.")
 
-# Define a message handler for handling user messages
 @Hiroko.on_message(filters.text)
-async def handle_message(client, message):
+async def handle_message(_, message):
     user_input = message.text
     response = await chat_with_pi(user_input)
     await message.reply(response)

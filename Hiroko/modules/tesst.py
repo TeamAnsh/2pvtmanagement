0c6b12916a935616c976d4c29c430104a489e0a4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-18 09:56:50 +0530

    Update tesst.py

--- Hiroko/modules/tesst.py
+++ Hiroko/modules/tesst.py
@@ -1,3 +1,215 @@
+
+import os, aiofiles, aiohttp, ffmpeg, random, textwrap, re
+import numpy as np
+import requests
+from os import path
+from Hiroko import Hiroko, pytgcalls, userbot
+from typing import Callable
+from pyrogram import filters, Client
+from pyrogram.types import *
+from youtube_search import YoutubeSearch
+from asyncio.queues import QueueEmpty
+from PIL import ImageGrab
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
+from pyrogram.errors import UserAlreadyParticipant
+from Hiroko.Helper.requirements import queues, converter, downloader, DurationLimitError, get_url, get_file_name            
+from Hiroko.Helper.admins import admins as a, set
+from pytgcalls import StreamType
+from pytgcalls.types.input_stream import InputStream
+from pytgcalls.types.input_stream import AudioPiped
+
+
+DURATION_LIMIT = float(300)
+
+keyboard = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton("50%", callback_data="volume_50"),
+            InlineKeyboardButton("100%", callback_data="volume_100"),
+        ],
+        [      
+            InlineKeyboardButton("150%", callback_data="volume_150"),
+            InlineKeyboardButton("200%", callback_data="volume_200")   
+        ]
+    ])
+
+
+
+
+
+que = {}
+
+chat_id = None
+useer = "NaN"
+
+
+def make_col():
+    return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
+
+def transcode(filename):
+    ffmpeg.input(filename).output(
+        "input.raw", format="s16le", acodec="pcm_s16le", ac=2, ar="48k"
+    ).overwrite_output().run()
+    os.remove(filename)
+
+
+
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
+
+def truncate(text):
+    list = text.split(" ")
+    text1 = ""
+    text2 = ""    
+    for i in list:
+        if len(text1) + len(i) < 27:        
+            text1 += " " + i
+        elif len(text2) + len(i) < 25:        
+            text2 += " " + i
+
+    text1 = text1.strip()
+    text2 = text2.strip()     
+    return [text1,text2]
+
+
+
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
+
+
+async def generate_cover(requested_by, title, views, duration, thumbnail):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open("background.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+
+    image = Image.open(f"./background.png")
+    black = Image.open("Hiroko/Helper/resources/black.jpg")
+    img = Image.open("Hiroko/Helper/resources/music.png")
+    image5 = changeImageSize(1280, 720, img)
+    image1 = changeImageSize(1280, 720, image)
+    image1 = image1.filter(ImageFilter.BoxBlur(10))
+    image11 = changeImageSize(1280, 720, image)
+    image1 = image11.filter(ImageFilter.BoxBlur(20))
+    image1 = image11.filter(ImageFilter.BoxBlur(20))
+    image2 = Image.blend(image1,black,0.6)
+
+    im = image5
+    im = im.convert('RGBA')
+    color = make_col()
+
+    data = np.array(im)
+    red, green, blue, alpha = data.T
+
+    white_areas = (red == 255) & (blue == 255) & (green == 255)
+    data[..., :-1][white_areas.T] = color
+
+    im2 = Image.fromarray(data)
+    image5 = im2
+
+
+    image3 = image11.crop((280,0,1000,720))
+    lum_img = Image.new('L', [720,720] , 0)
+    draw = ImageDraw.Draw(lum_img)
+    draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
+    img_arr =np.array(image3)
+    lum_img_arr =np.array(lum_img)
+    final_img_arr = np.dstack((img_arr,lum_img_arr))
+    image3 = Image.fromarray(final_img_arr)
+    image3 = image3.resize((600,600))
+    
+    image2.paste(image3, (50,70), mask = image3)
+    image2.paste(image5, (0,0), mask = image5)
+
+    
+    font1 = ImageFont.truetype(r'Hiroko/Helper/resources/robot.otf', 30)
+    font2 = ImageFont.truetype(r'Hiroko/Helper/resources/robot.otf', 60)
+    font3 = ImageFont.truetype(r'Hiroko/Helper/resources/robot.otf', 49)
+    font4 = ImageFont.truetype(r'Hiroko/Helper/resources/hiroko.ttf', 35)
+
+    image4 = ImageDraw.Draw(image2)
+    image4.text((10, 10), "HIROKO MUSIC", fill="white", font = font1, align ="left") 
+    image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+
+    
+    title1 = truncate(title)
+    image4.text((670, 280), text=title1[0], fill="white", font = font3, align ="left") 
+    image4.text((670, 332), text=title1[1], fill="white", font = font3, align ="left") 
+
+    
+    views = f"Views : {views}"
+    duration = f"Duration : {duration} minutes"
+    channel = f"Channel : T-Series"
+
+
+    
+    image4.text((670, 410), text=views, fill="white", font = font4, align ="left") 
+    image4.text((670, 460), text=duration, fill="white", font = font4, align ="left") 
+    image4.text((670, 510), text=channel, fill="white", font = font4, align ="left")
+
+    
+    image2.save(f"final.png")
+    os.remove(f"background.png")
+    final = f"temp.png"
+    return final
+
+
+@Hiroko.on_message(filters.command(["yt", "play"], prefixes=["/", "!"]))    
+async def play(_, message: Message):
+    global que
+    global useer
+    
+    lel = await message.reply("**🔎 sᴇᴀʀᴄʜɪɴɢ...**")
+   
+    bsdk = message.from_user.mention    
+    audio = (
+        (message.reply_to_message.audio or message.reply_to_message.voice)
+        if message.reply_to_message
+        else None
+    )
+    url = get_url(message)
+
+    if audio:
+        if round(audio.duration / 60) > DURATION_LIMIT:
+            raise DurationLimitError(
+                f"**» sᴏɴɢ ʟᴏɴɢᴇʀ ᴛʜᴀɴ {DURATION_LIMIT} ᴍɪɴᴜᴛᴇ's ᴀʀᴇ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴘʟᴀʏ.**"
+            )
+
+        file_name = get_file_name(audio)
+        title = file_name
+        thumb_name = "https://telegra.ph/file/00411492c1fb4c0a91f18.jpg"
+        thumbnail = thumb_name
+        duration = round(audio.duration / 60)
+        views = "Locally added"
+
+        
+
+        requested_by = message.from_user.first_name
+        await generate_cover(requested_by, title, views, duration, thumbnail)
+        file_path = await converter(
+            (await message.reply_to_message.download(file_name))
+            if not path.isfile(path.join("downloads", file_name))
+            else file_name
+        )
+
+
+""""
 import os, asyncio, re
 from config import OWNER_ID
 from pyrogram import filters
@@ -23,7 +235,7 @@ keyboard = InlineKeyboardMarkup([
         ]
     ])
 
-
+"""
 
 @Hiroko.on_message(filters.video_chat_started)
 async def brah(_, msg):
@@ -65,7 +277,7 @@ async def on_stream_end(chat_id):
     
 
 
-
+"""
 @Hiroko.on_message(filters.command(["play"], prefixes=["/", "!"]))
 async def play(_, msg: Message):
     try:
@@ -94,7 +306,7 @@ async def play(_, msg: Message):
     except Exception as e:         
         await msg.reply(f"Oops! Something went wrong {e}.")
 
-
+"""
 
 
 

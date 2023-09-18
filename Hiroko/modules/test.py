18fa5ba044ac9ec58cd26e391b74c68f31760270
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-19 01:43:54 +0530

    Update test.py

--- Hiroko/modules/test.py
+++ Hiroko/modules/test.py
@@ -8,40 +8,36 @@ from io import BytesIO
 
 
 
-# Function to convert text to audio
+active_calls = {}
+
 def text_to_audio(text):
     tts = gTTS(text=text, lang='en')
-    audio_file_path = 'output.mp3'
-    tts.save(audio_file_path)
-    return audio_file_path
+    file_path = 'output.mp3'
+    tts.save(file_path)
+    return file_path
 
-# Function to play audio in a voice chat
-async def play_audio_in_voice_chat(chat_id, audio_file_path):
-    with open(audio_file_path, "rb") as f:
-        audio_data = f.read()
-    
-    input_audio_stream = InputStream(
-        BytesIO(audio_data)
-    )
 
-    await pytgcalls.join_group_call(
-        chat_id,
-        InputAudioStream,
-        stream_type=StreamType().local_stream,
-    )
 
-# Define a command handler for /audio
 @Hiroko.on_message(filters.command("audio", prefixes="/"))
 async def audio_command(client, message):
     try:
-        chat_id = message.chat.id
-        text = message.text.split(None, 1)[1]
-        
-        # Convert the text to audio
-        audio_file_path = text_to_audio(text)
-        
-        # Play the audio in the voice chat
-        await play_audio_in_voice_chat(chat_id, audio_file_path)
+        if message.chat.id not in active_calls:
+            text = message.text.split(None, 1)[1]
+            file_path = text_to_audio(text)
+            with open(file_path, "rb") as f:
+                audio_data = f.read()
+            group_call = await pytgcalls.join_group_call(
+                message.chat.id,
+                InputStream(
+                    InputAudioStream(
+                        BytesIO(audio_data),
+                    ),
+                ),
+                stream_type=StreamType().local_stream,
+            )
+            active_calls[message.chat.id] = group_call
+        else:
+            await message.reply_text("There is already an active audio stream in this chat.")
     except IndexError:
         await message.reply_text("Please provide text to convert to audio using /audio.")
 

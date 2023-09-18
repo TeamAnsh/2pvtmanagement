4cc3416e6aa65dad0dc24de229d3a7d3fd5e2691
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-19 01:23:09 +0530

    Update test.py

--- Hiroko/modules/test.py
+++ Hiroko/modules/test.py
@@ -4,33 +4,46 @@ from Hiroko import Hiroko, pytgcalls
 from pyrogram import filters
 from pytgcalls import StreamType
 from pytgcalls.types.input_stream import InputStream
-from pytgcalls.types.input_stream import AudioPiped
+from io import BytesIO
 
 
 
-
+# Function to convert text to audio
 def text_to_audio(text):
     tts = gTTS(text=text, lang='en')
     audio_file_path = 'output.mp3'
     tts.save(audio_file_path)
     return audio_file_path
 
+# Function to play audio in a voice chat
 async def play_audio_in_voice_chat(chat_id, audio_file_path):
-    input_audio_stream = AudioPiped(
-        file_path=audio_file_path,
+    with open(audio_file_path, "rb") as f:
+        audio_data = f.read()
+    
+    input_audio_stream = InputStream(
+        BytesIO(audio_data)
     )
 
     await pytgcalls.join_group_call(
         chat_id,
-        InputStream(input_audio_stream),
+        input_audio_stream,
         stream_type=StreamType().local_stream,
     )
 
-
-
+# Define a command handler for /audio
 @Hiroko.on_message(filters.command("audio", prefixes="/"))
 async def audio_command(client, message):
-    text = message.text.split(None,1)[1]
-    audio_file_path = text_to_audio(text)  
-    await play_audio_in_voice_chat(message.chat.id, audio_file_path)
+    try:
+        # Extract the text after /audio
+        text = message.text.split(None, 1)[1]
+        
+        # Convert the text to audio
+        audio_file_path = text_to_audio(text)
+        
+        # Play the audio in the voice chat
+        await play_audio_in_voice_chat(message.chat.id, audio_file_path)
+    except IndexError:
+        await message.reply_text("Please provide text to convert to audio using /audio.")
+
+
 

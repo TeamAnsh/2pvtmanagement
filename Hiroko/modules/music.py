0064c51f4bb4ceaee358636015393d25a138649c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-19 22:30:14 +0530

    Update music.py

--- Hiroko/modules/music.py
+++ Hiroko/modules/music.py
@@ -356,16 +356,16 @@ async def skip(_, message: Message):
 @pytgcalls.on_stream_end()
 async def on_stream_end(_, update: Update) -> None:
     chat_id = update.chat_id
-    queues.task_done(chat_id)
+    rq.task_done(chat_id)
 
-    if queues.is_empty(chat_id):
+    if rq.is_empty(chat_id):
         await pytgcalls.leave_group_call(chat_id)
     else:
         await pytgcalls.change_stream(
             chat_id, 
             InputStream(
                 InputAudioStream(
-                    queues.get(chat_id)["file"],
+                    rq.get(chat_id)["file"],
                 ),
             ),
         )

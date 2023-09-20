d4d9b4d0d31175783e56a9e407533a3651992b2f
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-20 08:51:16 +0530

    Update waifus.py

--- Hiroko/modules/waifus.py
+++ Hiroko/modules/waifus.py
@@ -15,24 +15,23 @@ waifu_collection = db["waifus"]
 
 
 
-
 @Hiroko.on_message(filters.command(["addwaifu"]) & filters.user(SUDO_USERS))
 async def add_waifus(_, message):
     user_id = message.from_user.id    
-    waifu_photo await message.chat.ask("🌟 Great! Let's add a new waifu. Please send the waifu's photo as a reply to this message.")
+    waifu_photo = await message.chat.ask("🌟 Great! Let's add a new waifu. Please send the waifu's photo as a reply to this message.")
     photo = waifu_photo.photo[-1].file_id
     waifu_name = await message.chat.ask("Thanks! Now, please enter the waifu's name.")
-    name =waifu_name.text
+    name = waifu_name.text
     anime_name = await message.chat.ask("Got it! Please enter the anime the waifu is from.")
     anime = anime_name.text
-    waifu_rarity =  await message.chat.ask("Great! Finally, please enter the rarity of the waifu (e.g., common, rare, legendary).") 
+    waifu_rarity = await message.chat.ask("Great! Finally, please enter the rarity of the waifu (e.g., common, rare, legendary).") 
     rarity = waifu_rarity.text
     waifu_data = {
-                "waifu_photo": "photo",
-                "waifu_name": "name",
-                "waifu_anime": "anime",
-                "waifu_rarity": "rarity",
-            }
+        "waifu_photo": photo,
+        "waifu_name": name,
+        "waifu_anime": anime,
+        "waifu_rarity": rarity,
+    }
     await waifu_collection.insert_one(waifu_data)            
     await message.reply_text("🌟 Waifu added successfully! 🌟")
 

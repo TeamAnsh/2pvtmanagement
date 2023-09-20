d1e6b26e513851eeee3e912fbb44e92486632579
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-20 12:25:42 +0530

    Update waifus.py

--- Hiroko/modules/waifus.py
+++ Hiroko/modules/waifus.py
@@ -1,43 +1,53 @@
-from config import SUDO_USERS
+import requests
+from io import BytesIO
+from config import SUDO_USERS, MONGO_URL
 from pyrogram import filters
-from pyrogram.types import *
-from config import MONGO_URL
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
-from pyrogram.types import Message
 from Hiroko import Hiroko
 
 
-mongo = MongoCli(MONGO_URL).Rankings
+mongo = MongoCli(MONGO_URL)
 db = mongo["waifu_bot"]
 waifu_collection = db["waifus"]
 
-button = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton(f"message.from_user.mention", url=f"message.from_user.username"),
-            InlineKeyboardButton("close", callback_data="maintainer_"),
-        ]                
-    ])
+button = InlineKeyboardMarkup([       
+    [  
+        InlineKeyboardButton(f"message.from_user.mention", url=f"https://t.me/@{message.from_user.username}"),
+        InlineKeyboardButton("Close", callback_data="maintainer_"),
+    ]
+])
 
 
 
 @Hiroko.on_message(filters.command(["addwaifu"]) & filters.user(SUDO_USERS))
 async def add_waifus(_, message):
     if len(message.command) < 2:
-        return await message.reply("💌 **hello hottie ..**")
-            
+        return await message.reply("💌 Hello hottie, please provide the waifu details in the format: /addwaifu photo-name-anime-rarity")
     waifu_text = message.text.split(None, 1)[1]
     waifu = waifu_text.split("-")
-    waifu_photo = waifu[0]
-    waifu_name = waifu[1]
-    waifu_anime = waifu[2]
-    waifu_rarity = waifu[3]
+    if len(waifu) != 4:
+        return await message.reply("❌ Invalid format. Please provide waifu details in the format: /addwaifu photo-name-anime-rarity")
+
+    waifu_photo_url, waifu_name, waifu_anime, waifu_rarity = waifu
+
+    response = requests.get(waifu_photo_url)
+    if response.status_code != 200:
+        return await message.reply("❌ Failed to download the image from the URL.")
+
+    waifu_photo_bytes = BytesIO(response.content)
+
     waifu_data = {
-        "waifu_photo": waifu_photo,
+        "waifu_photo": waifu_photo_bytes.getvalue(),  # Store as bytes
         "waifu_name": waifu_name,
         "waifu_anime": waifu_anime,
         "waifu_rarity": waifu_rarity,
     }
+
     await waifu_collection.insert_one(waifu_data)
-    await Hiroko.send_photo(-1001936480103, photo=waifu_photo, reply_markup=button)
+
+    await Hiroko.send_photo(chat_id=-1001936480103, photo=waifu_photo_bytes, reply_markup=button)
     await message.reply_text("🌟 Waifu added successfully! 🌟")
+
+
 

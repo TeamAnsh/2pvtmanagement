590db58c5002f0a74c83fcc8156f85402715465a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-21 10:30:08 +0530

    Update test.py

--- Hiroko/modules/test.py
+++ Hiroko/modules/test.py
@@ -13,22 +13,43 @@ collection = db["chatgpt_settings"]
 
 openai.api_key = "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"
 
-
-
 @Hiroko.on_message(filters.command(["setmode"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
-async def set_mode(hiroko, message):    
-        chat_id = message.chat.id
-        if len(message.command) < 2:
-            await message.reply_text("Please specify a mode: /setmode audio or /setmode text")
-        else:
-            mode = message.command[1].lower()
-            if mode == "audio" or mode == "text":
-                set_chat_mode(chat_id, mode)
-                await message.reply_text(f"Chat mode set to: {mode}")
-            else:
-                await message.reply_text("Invalid mode. Please use /setmode audio or /setmode text")
-
+async def set_mode(hiroko, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    is_admin = await hiroko.get_chat_member(chat_id, user_id)
     
+    if not is_admin or is_admin.status not in ("creator", "administrator"):
+        await message.reply_text("Only group admins can change the mode.")
+        return
+
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Audio Mode", callback_data="mode_audio"),
+                InlineKeyboardButton("Text Mode", callback_data="mode_text"),
+            ]
+        ]
+    )
+
+    await message.reply_text("Please select a mode:", reply_markup=buttons)
+
+
+@Hiroko.on_callback_query(filters.regex("^mode_"))
+async def set_mode_callback(hiroko, callback_query):
+    chat_id = callback_query.message.chat.id
+    user_id = callback_query.from_user.id
+    mode = callback_query.data.split("_")[1]
+
+    is_admin = await hiroko.get_chat_member(chat_id, user_id)
+    
+    if not is_admin or is_admin.status not in ("creator", "administrator"):
+        await callback_query.answer("Only group admins can change the mode.")
+        return
+
+    set_chat_mode(chat_id, mode)
+    await callback_query.answer(f"Chat mode set to: {mode}")
+
 @Hiroko.on_message(filters.command(["assistant"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
 async def chat(hiroko, message):
     try:
@@ -49,44 +70,6 @@ async def chat(hiroko, message):
 
     except Exception as e:
         await message.reply_text(f"Error: {e}")
-
-
-
-
-async def process_question_text(hiroko, question, chat_id):
-    MODEL = "gpt-3.5-turbo"
-    resp = openai.ChatCompletion.create(model=MODEL, messages=[{"role": "user", "content": question}], temperature=0.2)
-    response_text = resp['choices'][0]["message"]["content"]
-    await hiroko.send_message(chat_id, response_text)
-
-async def process_question_audio(hiroko, question, chat_id):
-    MODEL = "gpt-3.5-turbo"
-    resp = openai.ChatCompletion.create(model=MODEL, messages=[{"role": "user", "content": question}], temperature=0.2)
-    response_text = resp['choices'][0]["message"]["content"]
-    
-    # Convert the AI response to audio
-    tts = gTTS(text=response_text, lang='en')
-    audio_file_name = "assistant_response.mp3"
-    tts.save(audio_file_name)
-    
-    # Send the audio response to the user
-    await hiroko.send_audio(chat_id, audio=audio_file_name, caption="Here's your voice answer message!")
-    
-    # Clean up the temporary audio file
-    os.remove(audio_file_name)
-
-def get_chat_mode(chat_id):
-    # Retrieve the chat mode from the database, default to "text"
-    chat_mode = collection.find_one({"chat_id": chat_id})
-    if chat_mode:
-        return chat_mode.get("mode", "text")
-    else:
-        # Default to "text" mode if not found in the database
-        return "text"
-
-def set_chat_mode(chat_id, mode):
-    # Update the chat mode in the database
-    collection.update_one({"chat_id": chat_id}, {"$set": {"mode": mode}}, upsert=True)
 
 
 

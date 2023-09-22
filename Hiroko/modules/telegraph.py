e6e0e3b3d5c5205e37ed676e522f77d667dcf410
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-22 13:17:26 +0530

    Update telegraph.py

--- Hiroko/modules/telegraph.py
+++ Hiroko/modules/telegraph.py
@@ -5,13 +5,11 @@ from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-# ------------------------------------------------------------------------------- #
-
 @Hiroko.on_message(filters.command(["tg", "tgm", "telegraph"], prefixes=["/", "!"]))
-async def telegraph(_, message: Message):    
+async def telegraph_command(_, message: Message):
     replied = message.reply_to_message
     if not replied:
-        await message.reply_text("reply to a supported media file")
+        await message.reply_text("Please reply to a supported media file.")
         return
     if not (
         (replied.photo and replied.photo.file_size <= 5242880)
@@ -29,25 +27,33 @@ async def telegraph(_, message: Message):
             and replied.document.file_size <= 5242880
         )
     ):
-        await message.reply_text("not supported!")
+        await message.reply_text("Unsupported file format!")
         return
     download_location = await Hiroko.download_media(
         message=message.reply_to_message, file_name="root/nana/"
     )
     try:
         response = upload_file(download_location)
-        buttons = [        
-            [            
-                InlineKeyboardButton("Telegraph", url=f"https://telegra.ph{response[0]}"),         
-                InlineKeyboardButton("Share", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")           
-            ]        
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    "Telegraph", url=f"https://telegra.ph{response[0]}"
+                ),
+                InlineKeyboardButton(
+                    "Share",
+                    url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}",
+                ),
+            ]
         ]
-        reply_markup = InlineKeyboardMarkup(buttons)            
-        await message.reply_text(f"**ʜᴇʟʟᴏ {message.from_user.mention}**\n**ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ [🥪](https://telegra.ph{response[0]})**", reply_markup=buttons)
-    except Exception as document:
-        await Hiroko.send_message(message.chat.id, document)
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            f"**Hello {message.from_user.mention}!**\n**Here is your Telegraph Link:** [🌿](https://telegra.ph{response[0]})",
+            reply_markup=reply_markup,
+        )
+    except Exception as err:
+        await Hiroko.send_message(message.chat.id, f"Error: {err}")
     finally:
         os.remove(download_location)
-        
-# ------------------------------------------------------------------------------- #
+
+
 

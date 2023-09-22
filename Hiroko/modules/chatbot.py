7c788b608df4a396bf5c78deab1d4d6bd28ec32d
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-22 20:47:27 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -1,1 +1,58 @@
+import requests
+import openai
+from Hiroko import *
+from pyrogram import * 
+from pyrogram.types import *
+from Hiroko.Helper.database import *
+
+
+openai.api_key = "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"
+
+completion = openai.Completion()
+
+
+start_sequence = "\nHiroko:"
+restart_sequence = "\nPerson:"
+session_prompt = chatbot_txt
+
+session = {}
+
+def ask(question, chat_log=None):
+    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt_text,
+        temperature=0.8,
+        max_tokens=250,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.3,
+        stop=["\n"],
+    )
+    story = response['choices'][0]['text']
+    return str(story)
+
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = session_prompt
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
+
+      
+
+@Natasha.on_message(filters.text, group=200)
+async def chatbot_reply(Hiroko :Hiroko, message):
+    bot_id = (await hiroko.get_me()).id
+    reply = message.reply_to_message
+    if reply and reply.from_user.id == bot_id:
+        q = message.text
+        try:
+            chat_log = session.get('chat_log')
+            answer = ask(q, chat_log)
+            session['chat_log'] = append_interaction_to_chat_log(Message, answer, chat_log)
+            await message.reply(f"{str(answer)}", quote=True)
+        except Exception as e:
+            return await message.reply("I can't answer that.")        
+
+
 

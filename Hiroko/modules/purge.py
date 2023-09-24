259bb3ff69019807b624f74ff5b2ba9e1c8c18dc
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-25 02:04:48 +0530

    Update purge.py

--- Hiroko/modules/purge.py
+++ Hiroko/modules/purge.py
@@ -27,10 +27,7 @@ async def purge(hiroko: Hiroko, msg: Message):
 
         try:
             for plist in m_list:
-                await hiroko.delete_messages(
-                    chat_id=msg.chat.id,
-                    message_ids=plist,
-                    revoke=True,)
+                await hiroko.delete_messages(chat_id=msg.chat.id, message_ids=plist, revoke=True)
                 
             await msg.delete()
         except MessageDeleteForbidden:
@@ -52,14 +49,14 @@ async def purge(hiroko: Hiroko, msg: Message):
 
 
 @Hiroko.on_message(filters.command("spurge") & admin_filter)
-async def spurge(c: Hiroko, m: Message):
+async def spurge(hiroko: Hiroko, msg: Message):
 
-    if m.chat.type != ChatType.SUPERGROUP:
-        await m.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
+    if msg.chat.type != ChatType.SUPERGROUP:
+        await msg.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
         return
 
-    if m.reply_to_message:
-        message_ids = list(range(m.reply_to_message.id, m.id))
+    if msg.reply_to_message:
+        message_ids = list(range(msg.reply_to_message.id, msg.id))
 
         def divide_chunks(l: list, n: int = 100):
             for i in range(0, len(l), n):
@@ -69,33 +66,29 @@ async def spurge(c: Hiroko, m: Message):
 
         try:
             for plist in m_list:
-                await c.delete_messages(
-                    chat_id=m.chat.id,
-                    message_ids=plist,
-                    revoke=True,
-                )
-            await m.delete()
+                await hiroko.delete_messages(chat_id=msg.chat.id, message_ids=plist, revoke=True)
+            await msg.delete()
         except MessageDeleteForbidden:
-            await m.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴅᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇs. ᴛʜᴇ ᴍᴇssᴀɢᴇs ᴍᴀʏ ʙᴇ ᴛᴏᴏ ᴏʟᴅ, ɪ ᴍɪɢʜᴛ ɴᴏᴛ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇ ʀɪɢʜᴛs, ᴏʀ ᴛʜɪs ᴍɪɢʜᴛ ɴᴏᴛ ʙᴇ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ.**")
+            await msg.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴅᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇs. ᴛʜᴇ ᴍᴇssᴀɢᴇs ᴍᴀʏ ʙᴇ ᴛᴏᴏ ᴏʟᴅ, ɪ ᴍɪɢʜᴛ ɴᴏᴛ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇ ʀɪɢʜᴛs, ᴏʀ ᴛʜɪs ᴍɪɢʜᴛ ɴᴏᴛ ʙᴇ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ.**")
             return
             
         except RPCError as ef:
-            await m.reply_text(text=f"**sᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀᴇᴅ, ʀᴇᴘᴏʀᴛ ɪᴛ ᴜsɪɴɢ** `/bug`<b>ᴇʀʀᴏʀ:</b> <code>{ef}</code>")           
-        return        
-    await m.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢᴇ !**")
+            await msg.reply_text(text=f"**sᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀᴇᴅ, ʀᴇᴘᴏʀᴛ ɪᴛ ᴜsɪɴɢ** `/bug`<b>ᴇʀʀᴏʀ:</b> <code>{ef}</code>")           
+            return        
+    await msg.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢᴇ !**")
     return
 
 
 @Hiroko.on_message(filters.command("del") & admin_filter)
-async def del_msg(c: Hiroko, m: Message):
-    if m.chat.type != ChatType.SUPERGROUP:
-        await m.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
+async def del_msg(hiroko: Hiroko, msg: Message):
+    if msg.chat.type != ChatType.SUPERGROUP:
+        await msg.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
         return        
-    if m.reply_to_message:
-        await m.delete()
-        await c.delete_messages(chat_id=m.chat.id, message_ids=m.reply_to_message.id)
+    if msg.reply_to_message:
+        await msg.delete()
+        await hiroko.delete_messages(chat_id=m.chat.id, message_ids=m.reply_to_message.id)
     else:
-        await m.reply_text(text="**ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ.**")
-    return
+        await msg.reply_text(text="**ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ.**")
+        return
 
 

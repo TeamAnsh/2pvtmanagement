497f2f2d962eb98054fee83a29a422c4ef9d8c4a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-25 23:13:58 +0530

    Update eval.py

--- Hiroko/modules/eval.py
+++ Hiroko/modules/eval.py
@@ -134,3 +134,44 @@ async def forceclose_command(_, CallbackQuery):
         return
 
 
+
+
+@Hiroko.on_message(filters.command(["sh","shell"]))
+async def shell(client, message):
+    if message.from_user.id not in OWNER_ID:
+        return
+    cmd = message.text.split(" ")
+    if len(cmd) == 1:
+        await message.reply_text("**ɴᴏ ᴄᴏᴍᴍᴀɴᴅ ᴡᴀs ɢɪᴠᴇɴ ᴛᴏ ʀᴜɴ!**")
+        return
+    cmd = message.text.split(None, 1)[1]
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    stdout, stderr = process.communicate()
+    reply = ""
+    stderr = stderr.decode()
+    stdout = stdout.decode()
+    if stdout:
+        reply += f"sᴛᴅᴏᴜᴛ\n{stdout}\n"
+    if stderr:
+        reply += f"sᴛᴅᴇʀʀ\n{stderr}\n"
+    if len(reply) > 3000:
+        with open("shell_output.txt", "w") as file:
+            file.write(reply)
+        with open("shell_output.txt", "rb") as doc:
+            await messsage.reply_document(
+                document=doc,
+                caption="sʜᴇʟʟ ᴏᴜᴛᴘᴜᴛ"
+            )
+        if os.path.isfile("shell_ouput.txt"):
+            os.remove("shell_output.txt")
+    else:
+        await message.reply_text(reply)
+
+
+
+

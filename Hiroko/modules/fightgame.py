f8ed4d8efeefc22cd3f1a77d9fbd6d2cac41200d
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-28 22:43:41 +0530

    Update fightgame.py

--- Hiroko/modules/fightgame.py
+++ Hiroko/modules/fightgame.py
@@ -63,13 +63,13 @@ def select_character(_, message):
             reply_markup=btn
         )
 
-    if user_id in user_profiles and user_profiles[user_id]["character"] is not None:
-        message.reply_text("You've already selected a character. You cannot change it.")
-        return
+        if user_id in user_profiles and user_profiles[user_id]["character"] is not None:
+            message.reply_text("You've already selected a character. You cannot change it.")
+            return
 
-    user_profiles[user_id] = {"character": None, "health": 100}
-    user_profiles[user_id]["character_page"] = 0  # Initialize character page to 0
-    message.reply_photo(photo="https://telegra.ph/file/061d8efe5247272458cb0.jpg", caption="Welcome to the Jujutsu Kaisen fighting game! Choose your character:", reply_markup=get_character_selection_keyboard(user_id))
+        user_profiles[user_id] = {"character": None, "health": 100}
+        user_profiles[user_id]["character_page"] = 0  # Initialize character page to 0
+        message.reply_photo(photo="https://telegra.ph/file/061d8efe5247272458cb0.jpg", caption="Welcome to the Jujutsu Kaisen fighting game! Choose your character:", reply_markup=get_character_selection_keyboard(user_id))
 
 
 

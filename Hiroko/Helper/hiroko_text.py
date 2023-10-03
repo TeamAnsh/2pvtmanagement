3b82930973252bc31bc896b31578a9942b2f0259
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-03 16:34:08 +0530

    Update and rename hiroko_text.txt to hiroko_text.py

---
+++ Hiroko/Helper/hiroko_text.py
@@ -0,0 +1,166 @@
+# ---------------------------------------------------------------------- #
+
+ACTION_TEXT = """
+These are the available action commands:
+
+/ban - Bans a user from the group.
+/unban - Unbans a previously banned user.
+/mute - Mutes a user in the group.
+/unmute - Unmutes a previously muted user.
+/kick - Kicks a user from the group.
+/promote - Promotes a user to an admin.
+/demote - Demotes an admin back to a regular user.
+/locks - Display the lock settings of the group.
+/nightmode - Enables or disables night mode in the group.
+/purge - Deletes messages up to the replied message.
+/spurge - Deletes messages up to the replied message without a success message.
+/del - Deletes a single message (should be used as a reply to the message).
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+AFK_TEXT = """
+These are the available AFK (Away From Keyboard) commands:
+
+/afk - Sets your status as AFK.
+/brb - Sets your status as BRB (Be Right Back).
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+WELCOME_TEXT = """
+These are the available welcome message commands:
+
+/setwelcome - Sets the welcome message for new members in the group.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+GAMES_TEXT = """
+These are the available game commands:
+
+/bonus - Gives a bonus point.
+/rewards - Shows the reward options.
+/bet - Places a bet on a game.
+/dart - Play a game of dart.
+/bowl - Play a game of bowling.
+/basket - Play a game of basketball.
+/pay - Pays a user from your balance.
+/balance - Checks your current balance.
+/leaderboard - Displays the leaderboard for the games.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+IMAGES_TEXT = """
+These are the available image commands:
+
+/draw - Generates a drawing based on a given keyword.
+/image - Searches for an image based on a given keyword.
+/upscale - Reply to an image to upscale it and improve its quality.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+GROUP_TEXT = """
+These are the available group management commands:
+
+/pin - Pins a message in the group.
+/pinned - Displays the pinned message in the group.
+/unpin - Unpins the currently pinned message.
+/staff - Displays the list of staff members.
+/bots - Displays the list of bots in the group.
+/settitle - Sets the title of the group.
+/setdescription - Sets the description of the group.
+/setphoto - Sets the group photo.
+/removephoto - Removes the group photo.
+/zombies - Removes inactive members from the group.
+/tagall - Tags all members in the group.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+STICKER_TEXT = """
+These are the available sticker commands:
+
+/packkang - Creates a pack of stickers from a series of images.
+/stickerid - Gets the sticker ID of a sticker.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+MISC_TEXT = """
+These are the available miscellaneous commands:
+
+/info - Displays information about the group.
+/github - Searches for a GitHub repository.
+/id - Displays your Telegram ID.
+/math - Solves mathematical problems and equations.
+/blackpink - Generates a Blackpink-style logo.
+/carbon - Generates a carbon code image from a code snippet.
+/speedtest - Measures the internet speed.
+/reverse - Reverses a given text.
+/webss - Takes a screenshot of a website.
+/paste - Uploads a text snippet to the cloud and gives a link.
+/tgm - Uploads a photo (under 5mb) to the cloud and gives a link.
+/tr - Translates text.
+/google - Searches for information on Google.
+/stack - Searches for programming-related information on Stack Overflow.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+IMPOSTER_TEXT = """
+These are the available imposter commands:
+
+/imposter on/off - Turns on or off the watcher for your group, which notifies about users who change their name or username.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+CHATGPT_TEXT = """
+These are the available ChatGPT commands:
+
+/ask - Queries the AI model to get a response to your question.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+MUSIC_TEXT = """
+These are the available music commands:
+
+!play - Plays a song or playlist.
+!stop - Stops the currently playing song.
+!end - Ends the music playback.
+!skip - Skips the current song.
+!volume - Adjusts the volume level.
+/song - Searches for a song.
+/video - Searches for a YouTube video.
+"""
+
+
+# ---------------------------------------------------------------------- #
+
+WAIFU_TEXT = """
+These are the available waifu commands:
+
+/grab - Grabs a random anime waifu image.
+/giftwaifu - Sends a random waifu image as a gift to another user.
+/mywaifu - Shows your current waifu.
+/waifu - Sets a new random waifu for yourself.
+"""
+
+# ---------------------------------------------------------------------- #
+
+
+

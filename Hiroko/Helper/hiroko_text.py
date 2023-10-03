795da10b6dd3c80d47af9e3582476a64e783035b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-03 16:50:09 +0530

    Update hiroko_text.py

--- Hiroko/Helper/hiroko_text.py
+++ Hiroko/Helper/hiroko_text.py
@@ -3,18 +3,18 @@
 ACTION_TEXT = """
 These are the available action commands:
 
-/ban - Bans a user from the group.
-/unban - Unbans a previously banned user.
-/mute - Mutes a user in the group.
-/unmute - Unmutes a previously muted user.
-/kick - Kicks a user from the group.
-/promote - Promotes a user to an admin.
-/demote - Demotes an admin back to a regular user.
-/locks - Display the lock settings of the group.
-/nightmode - Enables or disables night mode in the group.
-/purge - Deletes messages up to the replied message.
-/spurge - Deletes messages up to the replied message without a success message.
-/del - Deletes a single message (should be used as a reply to the message).
+⦿ /ban ➠ Bans a user from the group.
+⦿ /unban ➠ Unbans a previously banned user.
+⦿ /mute ➠ Mutes a user in the group.
+⦿ /unmute ➠ Unmutes a previously muted user.
+⦿ /kick ➠ Kicks a user from the group.
+⦿ /promote ➠ Promotes a user to an admin.
+⦿ /demote ➠ Demotes an admin back to a regular user.
+⦿./locks ➠ Display the lock settings of the group.
+⦿ /nightmode ➠ Enables or disables night mode in the group.
+⦿ /purge ➠ Deletes messages up to the replied message.
+⦿ /spurge ➠ Deletes messages up to the replied message without a success message.
+⦿ /del ➠ Deletes a single message (should be used as a reply to the message).
 """
 
 
@@ -23,8 +23,8 @@ These are the available action commands:
 AFK_TEXT = """
 These are the available AFK (Away From Keyboard) commands:
 
-/afk - Sets your status as AFK.
-/brb - Sets your status as BRB (Be Right Back).
+⦿ /afk ➠ Sets your status as AFK.
+⦿ /brb ➠ Sets your status as BRB (Be Right Back).
 """
 
 
@@ -33,7 +33,7 @@ These are the available AFK (Away From Keyboard) commands:
 WELCOME_TEXT = """
 These are the available welcome message commands:
 
-/setwelcome - Sets the welcome message for new members in the group.
+⦿ /setwelcome ➠ Sets the welcome message for new members in the group.
 """
 
 
@@ -42,15 +42,15 @@ These are the available welcome message commands:
 GAMES_TEXT = """
 These are the available game commands:
 
-/bonus - Gives a bonus point.
-/rewards - Shows the reward options.
-/bet - Places a bet on a game.
-/dart - Play a game of dart.
-/bowl - Play a game of bowling.
-/basket - Play a game of basketball.
-/pay - Pays a user from your balance.
-/balance - Checks your current balance.
-/leaderboard - Displays the leaderboard for the games.
+⦿ /bonus ➠ Gives a bonus point.
+⦿ /rewards ➠ Shows the reward options.
+⦿ /bet ➠ Places a bet on a game.
+⦿ /dart ➠ Play a game of dart.
+⦿ /bowl ➠ Play a game of bowling.
+⦿ /basket ➠ Play a game of basketball.
+⦿ /pay ➠ Pays a user from your balance.
+⦿ /balance ➠ Checks your current balance.
+⦿ /leaderboard ➠ Displays the leaderboard for the games.
 """
 
 
@@ -59,9 +59,9 @@ These are the available game commands:
 IMAGES_TEXT = """
 These are the available image commands:
 
-/draw - Generates a drawing based on a given keyword.
-/image - Searches for an image based on a given keyword.
-/upscale - Reply to an image to upscale it and improve its quality.
+⦿ /draw ➠ Generates a drawing based on a given keyword.
+⦿ /image ➠ Searches for an image based on a given keyword.
+⦿ /upscale ➠ Reply to an image to upscale it and improve its quality.
 """
 
 
@@ -70,17 +70,17 @@ These are the available image commands:
 GROUP_TEXT = """
 These are the available group management commands:
 
-/pin - Pins a message in the group.
-/pinned - Displays the pinned message in the group.
-/unpin - Unpins the currently pinned message.
-/staff - Displays the list of staff members.
-/bots - Displays the list of bots in the group.
-/settitle - Sets the title of the group.
-/setdescription - Sets the description of the group.
-/setphoto - Sets the group photo.
-/removephoto - Removes the group photo.
-/zombies - Removes inactive members from the group.
-/tagall - Tags all members in the group.
+⦿ /pin ➠ Pins a message in the group.
+⦿ /pinned ➠ Displays the pinned message in the group.
+⦿ /unpin ➠ Unpins the currently pinned message.
+⦿ /staff ➠ Displays the list of staff members.
+⦿ /bots ➠ Displays the list of bots in the group.
+⦿ /settitle ➠ Sets the title of the group.
+⦿ /setdescription ➠ Sets the description of the group.
+⦿ /setphoto ➠ Sets the group photo.
+⦿ /removephoto ➠ Removes the group photo.
+⦿ /zombies ➠ Removes inactive members from the group.
+⦿ /tagall ➠ Tags all members in the group.
 """
 
 
@@ -89,8 +89,8 @@ These are the available group management commands:
 STICKER_TEXT = """
 These are the available sticker commands:
 
-/packkang - Creates a pack of stickers from a series of images.
-/stickerid - Gets the sticker ID of a sticker.
+⦿ /packkang ➠ Creates a pack of stickers from a series of images.
+⦿ /stickerid ➠ Gets the sticker ID of a sticker.
 """
 
 
@@ -99,20 +99,20 @@ These are the available sticker commands:
 MISC_TEXT = """
 These are the available miscellaneous commands:
 
-/info - Displays information about the group.
-/github - Searches for a GitHub repository.
-/id - Displays your Telegram ID.
-/math - Solves mathematical problems and equations.
-/blackpink - Generates a Blackpink-style logo.
-/carbon - Generates a carbon code image from a code snippet.
-/speedtest - Measures the internet speed.
-/reverse - Reverses a given text.
-/webss - Takes a screenshot of a website.
-/paste - Uploads a text snippet to the cloud and gives a link.
-/tgm - Uploads a photo (under 5mb) to the cloud and gives a link.
-/tr - Translates text.
-/google - Searches for information on Google.
-/stack - Searches for programming-related information on Stack Overflow.
+⦿ /info ➠ Displays information about the group.
+⦿ /github ➠ Searches for a GitHub repository.
+⦿ /id ➠ Displays your Telegram ID.
+⦿ /math ➠ Solves mathematical problems and equations.
+⦿ /blackpink ➠ Generates a Blackpink-style logo.
+⦿ /carbon ➠ Generates a carbon code image from a code snippet.
+⦿ /speedtest ➠ Measures the internet speed.
+⦿ /reverse ➠ Reverses a given text.
+⦿ /webss ➠ Takes a screenshot of a website.
+⦿ /paste ➠ Uploads a text snippet to the cloud and gives a link.
+⦿ /tgm ➠ Uploads a photo (under 5mb) to the cloud and gives a link.
+⦿ /tr ➠ Translates text.
+⦿ /google ➠ Searches for information on Google.
+⦿ /stack ➠ Searches for programming-related information on Stack Overflow.
 """
 
 
@@ -121,7 +121,7 @@ These are the available miscellaneous commands:
 IMPOSTER_TEXT = """
 These are the available imposter commands:
 
-/imposter on/off - Turns on or off the watcher for your group, which notifies about users who change their name or username.
+⦿ /imposter on/off ➠ Turns on or off the watcher for your group, which notifies about users who change their name or username.
 """
 
 
@@ -130,7 +130,7 @@ These are the available imposter commands:
 CHATGPT_TEXT = """
 These are the available ChatGPT commands:
 
-/ask - Queries the AI model to get a response to your question.
+⦿ /ask ➠ Queries the AI model to get a response to your question.
 """
 
 
@@ -139,13 +139,13 @@ These are the available ChatGPT commands:
 MUSIC_TEXT = """
 These are the available music commands:
 
-!play - Plays a song or playlist.
-!stop - Stops the currently playing song.
-!end - Ends the music playback.
-!skip - Skips the current song.
-!volume - Adjusts the volume level.
-/song - Searches for a song.
-/video - Searches for a YouTube video.
+⦿ !play ➠ Plays a song or playlist.
+⦿ !stop ➠ Stops the currently playing song.
+⦿ !end ➠ Ends the music playback.
+⦿ !skip ➠ Skips the current song.
+⦿ !volume ➠ Adjusts the volume level.
+⦿ /song ➠ Searches for a song.
+⦿ /video ➠ Searches for a YouTube video.
 """
 
 
@@ -154,10 +154,10 @@ These are the available music commands:
 WAIFU_TEXT = """
 These are the available waifu commands:
 
-/grab - Grabs a random anime waifu image.
-/giftwaifu - Sends a random waifu image as a gift to another user.
-/mywaifu - Shows your current waifu.
-/waifu - Sets a new random waifu for yourself.
+⦿ /grab ➠ Grabs a random anime waifu image.
+⦿ /giftwaifu ➠ Sends a random waifu image as a gift to another user.
+⦿ /mywaifu ➠ Shows your current waifu.
+⦿ /waifu ➠ Sets a new random waifu for yourself.
 """
 
 # ---------------------------------------------------------------------- #

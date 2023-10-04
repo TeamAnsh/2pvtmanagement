7794de27eb2654abe0ae99fe4c270712920aeb1e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-04 17:44:42 +0530

    Update stats.py

--- Hiroko/modules/stats.py
+++ Hiroko/modules/stats.py
@@ -35,6 +35,22 @@ photo = [
 ]
 
 
+@Hiroko.on_message(group=10)
+async def chat_watcher_func(_, message):
+    if message.from_user:
+        us_in_db = await get_user(message.from_user.id)
+        if not us_in_db:
+            await add_user(message.from_user.id)
+
+    chat_id = (message.chat.id if message.chat.id != message.from_user.id else None)
+
+    if not chat_id:
+        return
+
+    in_db = await get_chat(chat_id)
+    if not in_db:
+        await add_chat(chat_id)
+
 
 # --------------------------------------------------------------------------------- #
 

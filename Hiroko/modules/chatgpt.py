8885553f1af6ab3afc5556bd1281a8231549fc9c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-09 21:13:16 +0530

    Update chatgpt.py

--- Hiroko/modules/chatgpt.py
+++ Hiroko/modules/chatgpt.py
@@ -3,7 +3,7 @@ import openai
 from pyrogram import filters
 from Hiroko import Hiroko
 from pyrogram.enums import ChatAction, ParseMode
-
+from gtts import gTTS
 
 
 
@@ -30,5 +30,36 @@ async def chat(hiroko :Hiroko, message):
             await message.reply_text(f"{x}")     
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ")        
+
+
+
+
+
+
+@Hiroko.on_message(filters.command(["assis","assstant"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+async def chat(hiroko :Hiroko, message):
+    
+    try:
+        start_time = time.time()
+        await hiroko.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "**ʜᴇʟʟᴏ sɪʀ**\n**ᴇxᴀᴍᴘʟᴇ:-**`.assis How to set girlfriend ?`")
+        else:
+            a = message.text.split(' ', 1)[1]
+            MODEL = "gpt-3.5-turbo"
+            resp = openai.ChatCompletion.create(model=MODEL,messages=[{"role": "user", "content": a}],
+    temperature=0.2)
+            x=resp['choices'][0]["message"]["content"]
+            text = x    
+            tts = gTTS(text, lang='en')
+            tts.save('output.mp3')
+            await hiroko.send_voice(chat_id=message.chat.id, voice='output.mp3')
+            os.remove('output.mp3')            
+            
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ")        
+
+
 
 

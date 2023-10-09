3dd9d6484b69f7575a50d9f53ee9de165c9e5116
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-09 18:39:46 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -113,7 +113,7 @@ def append_interaction_to_chat_log(question, answer, chat_log=None):
 
 @Hiroko.on_message(filters.text, group=200)
 async def chatbot_reply(hiroko: Hiroko, message):
-    bot_id = "6632922889"
+    bot_id = 6632922889
     reply = message.reply_to_message
     if reply and reply.from_user.id == bot_id:
         query = message.text
